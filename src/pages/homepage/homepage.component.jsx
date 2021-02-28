6d6d95be2c572e1ef161ba793348034e79657317
dceu mobile <dceu00@gmail.com>
2021-02-27 22:27:35 -0600

    feat: homepage

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homepage.styles.scss'
+const Homepage = () => (
+    <div className = 'homepage'>
+        <div className = 'directory'>
+            
+        </div>
+
+    </div>
+);
+
+export default Homepage;
