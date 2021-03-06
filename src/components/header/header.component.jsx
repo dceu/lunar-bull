39125783d133ab6e1ec6af5e537c082086bdf350
dceu mobile <dceu00@gmail.com>
2021-03-06 04:02:42 -0600

    header styles

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './header.styles.scss'
+
+const Header = () => {
+    <div className = 'header'>
+        <div className = 'logo'></div>
+        <div className = 'options'>
+            
+        </div>
+
+    </div>
+
+}
+
+export default Header
