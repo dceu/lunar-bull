05ca900f0531c2d5160384b184abfb59a268114f
dceu mobile <dceu00@gmail.com>
2021-03-11 12:38:54 -0600

    blog component and styles

---
+++ src/pages/blog/blog.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './blog.styles.scss'
+
+const Blog = () => 
+    (
+        <div className="blog-page">
+            <h1>Musings and more</h1>
+        </div>
+    )
+
+
+export default Blog;
