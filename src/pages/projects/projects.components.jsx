0be147a64595afbe6a158fa241a60ac5c8be0b7a
dceu mobile <dceu00@gmail.com>
2021-03-11 12:39:36 -0600

    projects page component and style

---
+++ src/pages/projects/projects.components.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './projects.styles.scss';
+
+const Projects = () => 
+    (
+        <div className = "projects-page">
+            <h1>Projects</h1>
+        </div>
+    )
+
+
+export default Projects;
