48f9d98530d4ff215dc1fdfb5f40a0a5543e15cb
dceu mobile <dceu00@gmail.com>
2021-03-11 12:37:42 -0600

    pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component.jsx';
+import Resume from './pages/resume/resume.component.jsx';
+import Bio from './pages/bio/bio.component.jsx';
+import Blog from './pages/blog/blog.component.jsx';
+import Contact from './pages/contact/contact.component.jsx';
+import Projects from './pages/projects/projects.components.jsx';
+import Header from './components/header/header.component';
 class App extends React.Component {
   constructor() {
     super();
@@ -11,11 +17,18 @@ class App extends React.Component {
   render() {
     return(
       <div> 
-        <div>header</div>
+        <Header />
         
+        <div className = 'content-container'>
         <Switch>
           <Route exact path='/' component = {HomePage} />
+          <Route path = '/resume' component = {Resume} />
+          <Route path = '/projects' component = {Projects} />
+          <Route path = '/blog' component = {Blog} />
+          <Route path = '/bio' component = {Bio} />
+          <Route path = '/contact' component ={Contact} />
         </Switch>
+        </div>
           
         
       </div>
