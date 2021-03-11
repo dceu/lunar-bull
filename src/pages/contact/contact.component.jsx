d6fd942aebe8276bb7817edc54bf77d3e0b457d2
dceu mobile <dceu00@gmail.com>
2021-03-11 12:39:09 -0600

    contact component and styles

---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './contact.styles.scss'
+
+const Contact = () => 
+    (
+        <div className = "contact-page">
+            <h1>Contact Me</h1>
+        </div>
+    )
+
+
+export default Contact;
