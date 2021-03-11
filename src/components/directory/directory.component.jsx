44111ed83d6c0eca6b8f7333be4595ff366a9416
dceu mobile <dceu00@gmail.com>
2021-03-11 12:38:00 -0600

    directory component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import {Link} from 'react-router-dom'
 class Directory extends React.Component {
     constructor() {
         super();
@@ -11,18 +12,28 @@ class Directory extends React.Component {
                 {
                     id: 0,
                     title: 'resume',
+                    url: '/resume',
+                    icon: '9784'
                 },{
                     id: 1,
                     title: 'projects',
+                    url: '/projects',
+                    icon: '9883'
                 },{
                     id: 2,
                     title: 'blog',
+                    url: '/blog',
+                    icon: '10081'
                 },{
                     id: 3,
                     title: 'bio',
+                    url: '/bio',
+                    icon: '9778'
                 },{
                     id: 4,
                     title: 'contact',
+                    url: '/contact',
+                    icon: '128172'
                 },
             ]        
         }
@@ -31,10 +42,16 @@ class Directory extends React.Component {
     render() {
         return(
             <div className = 'directory'>
-                {this.state.pages.map(({id, ...pageProps}) => (
-                    <MenuItem key = {id} {...pageProps} />
+                <h1>directory</h1>
+                {this.state.pages.map( page => (
+                    <div className="directory-option">
+                    
+                    <Link key = {page.id} to={page.url}>{page.title}</Link>
+                    </div>
                 ))}
             </div>
         )
     }
 }
+
+export default Directory;
