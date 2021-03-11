637db0d6d49064878eb27e43ba617fc9b32e1803
dceu mobile <dceu00@gmail.com>
2021-03-11 12:38:19 -0600

    header component and styles

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Directory from '../directory/directory.component'
 import './header.styles.scss'
 
-const Header = () => {
+const Header = () => (
     <div className = 'header'>
-        <div className = 'logo'></div>
+        <div className = 'logo'>
+            <Link className='home-utf' to='/'>&#8962;</Link></div>
         <div className = 'options'>
             
         </div>
+        <Directory />
 
     </div>
 
-}
+)
 
 export default Header
