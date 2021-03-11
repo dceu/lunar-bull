0c7e48f3143e71eb892bd96d947884756ed310e6
dceu mobile <dceu00@gmail.com>
2021-03-11 12:38:36 -0600

    bio component and styles

---
+++ src/pages/bio/bio.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './bio.styles.scss'
+
+const Bio = () => 
+    (
+        <div className="biography-page">
+            <h1>About</h1>
+        </div>
+    )
+
+
+export default Bio;
