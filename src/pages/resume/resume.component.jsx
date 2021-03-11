2dd41c940de3aa87c9fb5f9bafd4ff7cda07cd8d
dceu mobile <dceu00@gmail.com>
2021-03-11 12:39:53 -0600

    resume component and styles

---
+++ src/pages/resume/resume.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './resume.styles.scss'
+
+const Resume = () => 
+    (
+        <div className = "resume-page">
+            <h1>Resume Page</h1>
+        </div>
+    )
+
+
+export default Resume;
