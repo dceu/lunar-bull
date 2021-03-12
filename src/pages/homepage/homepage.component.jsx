679c3fc3460d3337e5a27f96f916a4512e122fac
dceu mobile <dceu00@gmail.com>
2021-03-12 00:40:11 -0600

    what is a React Developer

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ContentCard from '../../components/content-card/content-card.component.jsx'
 import Directory from '../../components/directory/directory.component.jsx'
 
 import './homepage.styles.scss'
 const Homepage = () => (
     <div className = 'homepage'>
-        <h1>homepage content</h1>
+        <h1>What is a React Developer?</h1>
+        <ContentCard />
 
     </div>
 );
