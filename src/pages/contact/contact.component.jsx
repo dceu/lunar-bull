8470f51486336189e46d1aea566b55b023f41292
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-06-02 17:48:21 -0500

    updated contact info page

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 import './contact.styles.scss'
+import ContentCard from '../../components/content-card/content-card.component'
 
 const Contact = () => 
     (
         <div className = "contact-page">
-            <h1>Contact Me</h1>
+            <h1>Contact</h1>
+            <ul>
+            <li><a href="mailto:donovanuy@gmaill.com">Email</a></li>
+            <li><a href="https://www.linkedin.com/in/donovanuy/">LinkedIn</a></li>
+            <li><a href="https://www.instagram.com/donovanuy/">Instagram</a></li>
+            </ul>
         </div>
     )
 
