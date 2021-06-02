8ccaccde42348961afad958343ee7f3d1404d6ee
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-06-02 17:47:37 -0500

    placeholder content card for future nextjs implementation

--- src/pages/blog/blog.component.jsx
+++ src/pages/blog/blog.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import './blog.styles.scss'
+import ContentCard from '../../components/content-card/content-card.component'
 
 const Blog = () => 
     (
         <div className="blog-page">
-            <h1>Musings and more</h1>
+            <h1>Musings aplenty</h1>
+            <ContentCard id='0' heading='A Blog powered by NextJS (or kin)'
+             body=
+             'How can I funnel NextJS output into this component?' />
         </div>
     )
 
