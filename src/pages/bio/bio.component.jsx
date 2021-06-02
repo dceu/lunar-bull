72c1ed623007be2c7443bc96ec6d97172e68af0c
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-06-02 17:47:04 -0500

    biography page update includes new profile asset

--- src/pages/bio/bio.component.jsx
+++ src/pages/bio/bio.component.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
 import './bio.styles.scss'
+import ContentCard from '../../components/content-card/content-card.component'
 
 const Bio = () => 
     (
         <div className="biography-page">
-            <h1>About</h1>
+
+            <img class='profile' src='assets/profile/profile.jpg'/>
+            
+        <ContentCard id='0' heading='Donovan Conrad Estrada Uy'
+         body='Born in Tampa, FL a stonethrow away from Raymond James Stadium (where the cannons sound as the Bucs play), and would much prefer if somebody else wrote his biography.'/>
         </div>
     )
 
