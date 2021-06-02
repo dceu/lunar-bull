caedddc411222668a43cb4a1391b1839a62da865
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-06-02 17:48:39 -0500

    commented out some lorem ipsum

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -12,29 +12,29 @@ class Homepage extends React.Component {
                 {
                     id: '1',
                     heading : 'This is a Website Built with ReactJS',
-                    inquiries: ['What will be a component?','Will it be stateless or functional?'],
+                    inquiries: [' What will be a component?',' Will it be stateless or functional?'],
         
                 },
                 {
                     id: '2',
                     heading: 'A React Dev decides on State',
-                    inquiries: ['What data will State represent?', 'Where will State be "stored"?', 'How will state be accessed?'],
+                    inquiries: [' What data will State represent?', ' Where will State be "stored"?', ' How will state be accessed?'],
                 },
                 {
                     id: '3',
                     heading: 'A React Dev designs Dynamism',
                     inquiries: ['What changes when state changes?'],
                 },
-                {
-                    id: '4',
-                    heading: 'Lorem',
-                    inquiries: ['Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aperiam omnis neque recusandae totam laudantium veniam maxime, sequi debitis animi? Voluptatum quis perspiciatis corporis autem in odio eaque beatae ad.'],
-                },
-                {
-                    id: '5',
-                    heading: 'Lorem',
-                    inquiries: ['Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aperiam omnis neque recusandae totam laudantium veniam maxime, sequi debitis animi? Voluptatum quis perspiciatis corporis autem in odio eaque beatae ad.'],
-                },
+                // {
+                //     id: '4',
+                //     heading: 'Lorem',
+                //     inquiries: ['Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aperiam omnis neque recusandae totam laudantium veniam maxime, sequi debitis animi? Voluptatum quis perspiciatis corporis autem in odio eaque beatae ad.'],
+                // },
+                // {
+                //     id: '5',
+                //     heading: 'Lorem',
+                //     inquiries: ['Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aperiam omnis neque recusandae totam laudantium veniam maxime, sequi debitis animi? Voluptatum quis perspiciatis corporis autem in odio eaque beatae ad.'],
+                // },
                 
             ]
         }
@@ -44,8 +44,8 @@ class Homepage extends React.Component {
         return(
             <div className = 'homepage'>
                 <h1 className= 'welcome'>"Hello World"</h1>
-                <h3> this is a subheading </h3>
-                <p>some basic text</p>
+                {/* <h3> this is a subheading </h3>
+                <p>some basic text</p> */}
                 <div className="content-cards">
                 {this.state.content.map( card => (
                     
