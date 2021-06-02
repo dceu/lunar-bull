b86a0612072a9a8da5fc9fefa109da43bb1e00cc
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-06-02 17:48:54 -0500

    updated resume page with new resume asset

--- src/pages/resume/resume.component.jsx
+++ src/pages/resume/resume.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import './resume.styles.scss'
+import ContentCard from '../../components/content-card/content-card.component'
 
 const Resume = () => 
     (
         <div className = "resume-page">
             <h1>Resume Page</h1>
-        </div>
+            <ContentCard id='0' heading='2021' body='Below is a jpg preview of a pdf'/>
+            <a href='/assets/resume/resume_2021_state.pdf'>
+                <img src='/assets/resume/resume_2021_state1024_1.jpg'></img>
+            </a>
+            </div>
     )
 
 
