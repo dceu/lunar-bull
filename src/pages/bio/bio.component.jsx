617a857aadbc7d2a45a2ca81a5ce08f7ece40f79
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-06-04 14:35:47 -0500

    bio-header wrapper and refactor for ContentCard functionality

--- src/pages/bio/bio.component.jsx
+++ src/pages/bio/bio.component.jsx
@@ -7,11 +7,15 @@ const Bio = () =>
     (
         <div className="biography-page">
 
-            <img class='profile' src='assets/profile/profile.jpg'/>
+            <div className="bio-header"><img class='profile' src='assets/profile/profile.jpg'/></div>
             
-        <ContentCard id='0' heading='Donovan Conrad Estrada Uy'
-         body='Born in Tampa, FL a stonethrow away from Raymond James Stadium (where the cannons sound as the Bucs play), and would much prefer if somebody else wrote his biography.'/>
+        <ContentCard id='0' heading='Donovan Conrad Estrada Uy'>
+            <p>Born in Tampa, FL a stonethrow away from Raymond James Stadium (where the cannons sound as the Bucs play), grew up when the art classes were being cut in lieu of windows suite literacy tutorials (playing The Oregon Trail).</p>
+            <p>Can be found practicing geometric symmetries in a boxing gym if not servicing tables for a middle-market.</p>
+            <p>Currently studying Project Management, and Data Science while trying to maintain a web-development skillset.</p>
+            </ContentCard>
         </div>
+        
     )
 
 
