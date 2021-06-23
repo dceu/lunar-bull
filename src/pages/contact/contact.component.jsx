c5be99a35c2781681232462edf653204ab37b6eb
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2021-06-23 15:59:37 -0500

    added github to contact component

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -10,6 +10,7 @@ const Contact = () =>
             <ul>
             <li><a href="mailto:donovanuy@gmaill.com">Email</a></li>
             <li><a href="https://www.linkedin.com/in/donovanuy/">LinkedIn</a></li>
+            <li><a href="https://www.github.com/dceu/">GitHub</a></li>
             <li><a href="https://www.instagram.com/donovanuy/">Instagram</a></li>
             </ul>
         </div>
