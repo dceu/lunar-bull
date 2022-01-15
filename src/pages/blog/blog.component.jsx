24d6abedcc85072f6cab621b404e90575e97e29c
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2022-01-15 17:49:49 -0500

    Blog as a class component

--- src/pages/blog/blog.component.jsx
+++ src/pages/blog/blog.component.jsx
@@ -3,15 +3,33 @@ import React from 'react'
 import './blog.styles.scss'
 import ContentCard from '../../components/content-card/content-card.component'
 
-const Blog = () => 
-    (
-        <div className="blog-page">
-            <h1>Musings aplenty</h1>
-            <ContentCard id='0' heading='A Blog powered by NextJS (or kin)'>
-            How can I funnel NextJS output into this component?
-                </ContentCard>
-        </div>
-    )
+// const Blog = () => 
+//     (
+//         <div className="blog-page">
+//             <h1>Musings aplenty</h1>
+//             <ContentCard id='0' heading='A Blog powered by NextJS (or kin)'>
+//             How can I funnel NextJS output into this component?
+//                 </ContentCard>
+//         </div>
+//     )
 
+import md from '/assets/blog/test.md';
+
+class Blog extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            content :[
+                
+            ]
+        }
+    }
+
+    // rolodex list of blog assets
+        // at page level
+        // fetch asset
+        // stringify asset
+        // pass to react-markdown
+}
 
 export default Blog;
