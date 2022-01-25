0e325d48597c94f1c4be021dc7ac466a09774e13
dceu <dceu00@gmail.com>
2022-01-25 17:44:35 +0000

    directly set state in this.state in the contructor, nested <Switch> and <div rolodex> in <BrowserRouter>

--- src/pages/blog/blog.component.jsx
+++ src/pages/blog/blog.component.jsx
@@ -1,37 +1,24 @@
 import React from 'react'
 import BlogPage from './blog-page/blog-page.component.jsx'
 import ContentCard from '../../components/content-card/content-card.component.jsx'
-import {Link, Route, BrowserRouter} from 'react-router-dom'
-import bloglist from '../../util/blog-list.json';
+import {Link, Route, BrowserRouter, Switch
+} from 'react-router-dom'
+import bloglist from '../../blog-src/blog-list.json';
 class Blog extends React.Component{
-    
     constructor(props){
         super(props);
         this.state = {
-            pages : ""        }
+            pages: bloglist
+            }
     }
-    
     componentDidMount(){
-        this.setState({
-            blogList : bloglist
-        })
+        const blString = JSON.stringify(this.state.blogList);
     }
-    
     render (){
-        const {blogList} = this.state;
-        const blString = JSON.stringify(blogList);
-       // make urls from list
-       let urls = new Array();
-      if (blogList) {urls.push(
-          blogList.map(str => '/blog/' + str.slice(0, -3))
-          ) }
-       //blogList.map( (str, idx )=> {
-        //  return urls[idx] = '/blog/' + str.slice(str.indexOf('.'));
-       //})
+        const {pages} = this.state;
         return(
         <div className ='container'>
         <div id ="blog-list">
-        
         <h1>This is the top of the Blog</h1>
         <p>
           glob the blogs before build,<br/>
@@ -39,21 +26,28 @@ class Blog extends React.Component{
           iterate over list to have markdown source to pass to blog-page<br/>
             blogList = glob blog-src/*.md
         <br/>
-            blogList = {blString}
         </p>
         </div>
         <BrowserRouter>
-        <div id= 'rolodex'>
-            {
-            console.log(blogList)
+           <Switch>
+            {   
+                pages.map((page, idx) => (
+               <Route exact path={`/blog/${page.fileName}`} >
+                <BlogPage mdSource = {page.fileName}/> 
+               </Route>
+                
+                ))
             }
+            </Switch>
+            <div id="rolodex">
             {
-               console.log(urls) 
-           } 
-            
-            
-        </div>
+                pages.map((page, idx) => (
+                <Link to={`/blog/${page.fileName}`} key = {idx}>{page.title}</Link>
+                ))
+            }
+            </div>
         </BrowserRouter>
+            
         </div>
         )
     }
