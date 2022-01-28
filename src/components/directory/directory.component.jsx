fbb167528742eb7fa0b11cc4f15aa734d3d70d45
dceu <dceu00@gmail.com>
2022-01-28 02:20:07 +0000

    turned Directory into a functional component, import {useState}, began toggleability functionality

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,52 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './directory.styles.scss'
 import {Link} from 'react-router-dom'
-class Directory extends React.Component {
-    constructor() {
-        super();
 
-        // from live to sun-tiger
-
-        this.state = {
-            pages: [
-                // {
-                //     id: 0,
-                //     title: 'resume',
-                //     url: '/resume',
-                //     icon: '9784'
-                // },
-                //{
-                //    id: 1,
-                //     title: 'projects',
-                //     url: '/projects',
-                //     icon: '9883'
-                // },{
-                //     id: 2,
-                //     title: 'blog',
-                //     url: '/blog',
-                //     icon: '10081'
-                // },
+const Directory = ({viewable}) => {
+        const [ isViewable, setViewable ] = useState(viewable);
+        const [pages ] = 
+        useState(
+           [
                 {
                     id: 0,
+                    title: 'blog',
+                    url: '/blog',
+                    icon: '10081'
+                },
+                {
+                    id: 1,
                     title: 'portfolio',
                     url: '/portfolio',
 
                 },
                 {
-                    id: 1,
+                    id: 2,
                     title: 'contact',
                     url: '/contact',
                     // icon: '128172'
                 },
             ]        
-        }
-    }
+        );
 
-    render() {
         return(
-            <div className = 'directory'>
+            <div className = {`directory ${isViewable ? "null": "hidden"}`}>
                 
-                {this.state.pages.map( page => (
+                {pages.map( page => (
                     <div className="directory-option">
                     
                     <Link key = {page.id} to={page.url}>{page.title.toUpperCase()}</Link>
@@ -54,7 +39,6 @@ class Directory extends React.Component {
                 ))}
             </div>
         )
-    }
 }
 
 export default Directory;
