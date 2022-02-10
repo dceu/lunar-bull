c88a78d020555ba82c725d7388b847832f64b1e0
dceu <dceu00@gmail.com>
2022-02-10 18:59:54 +0000

    removing <Switch> from blog component as it already exists at App.js level

--- src/pages/blog/blog.component.jsx
+++ src/pages/blog/blog.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import BlogPage from './blog-page/blog-page.component.jsx'
 import ContentCard from '../../components/content-card/content-card.component.jsx'
-import {Link, Route, BrowserRouter, Switch
+import {Link, Route, BrowserRouter, Switch, Router, withRouter
 } from 'react-router-dom'
 import bloglist from '../../blog-src/blog-list.json';
+import Drawer from '../../components/drawer/drawer.component.jsx'
 class Blog extends React.Component{
     constructor(props){
         super(props);
@@ -18,39 +19,31 @@ class Blog extends React.Component{
         const {pages} = this.state;
         return(
         <div className ='container'>
-        <div id ="blog-list">
-        <h1>This is the top of the Blog</h1>
-        <p>
-          glob the blogs before build,<br/>
-          make a json object that lists all the blogs,<br/>
-          iterate over list to have markdown source to pass to blog-page<br/>
-            blogList = glob blog-src/*.md
-        <br/>
-        </p>
-        </div>
-        <BrowserRouter>
-           <Switch>
-            {   
-                pages.map((page, idx) => (
-               <Route exact path={`/blog/${page.fileName}`} >
-                <BlogPage mdSource = {page.fileName}/> 
-               </Route>
-                
-                ))
-            }
-            </Switch>
-            <div id="rolodex">
-            {
-                pages.map((page, idx) => (
-                <Link to={`/blog/${page.fileName}`} key = {idx}>{page.title}</Link>
-                ))
-            }
-            </div>
-        </BrowserRouter>
             
+            <BrowserRouter>
+            <Drawer pages={pages} children=
+               {
+                   pages.map((page, idx) => (
+                   <Link to={`/blog/${page.fileName}`} key = {idx}>{page.title}</Link>
+                   ))
+               } 
+               />
+              
+               {   
+                   pages.map((page, idx) => (
+                  <Route path={`/blog/${page.fileName}`} >
+                   <BlogPage mdSource = {page.fileName}/> 
+                  </Route>
+                   
+                   ))
+               }
+               
+               <div id="rolodex">
+               </div>
+            </BrowserRouter>
         </div>
         )
     }
 }
 
-export default Blog;
+export default withRouter(Blog);
